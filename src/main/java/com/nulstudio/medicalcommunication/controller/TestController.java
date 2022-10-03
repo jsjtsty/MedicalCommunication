b12cf3418161042484e7860a647cff335c43f5b3
NULL <jsjtsty@126.com>
2022-10-03 14:10:18 +0800

    Fix Bug #3, 2022.10.3.

--- src/main/java/com/nulstudio/medicalcommunication/controller/TestController.java
+++ src/main/java/com/nulstudio/medicalcommunication/controller/TestController.java
@@ -5,11 +5,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 @Controller
 public class TestController {
     @RequestMapping("/")
     @ResponseBody
     public TestEntity index() {
-        return new TestEntity(System.getProperty("user.dir"));
+        Process proc;
+        StringBuilder s = new StringBuilder();
+        try {
+            proc = Runtime.getRuntime().exec("python ./src/main/python/CommunicationTest.py");
+            BufferedReader in = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+            String line = null;
+            while ((line = in.readLine()) != null) {
+                s.append(line).append('\n');
+            }
+            in.close();
+            proc.waitFor();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return new TestEntity(s.toString());
     }
 }
---
+++ src/main/python/CommunicationTest.py
@@ -0,0 +1,18 @@
+'''
+usr接收用户传来的内容
+后端调用ComTest函数并将返回值传回到前端
+
+'''
+
+def ComTest(text):
+    # print(text)
+    if '你好' in text or '您好' in text:
+        return '您好，请问有什么需要帮助的吗？'
+    elif '你说的不对' in text:
+        return '不好意思，您咨询的正在补充。'
+    elif '感谢' in text or '谢谢' in text:
+        return '不客气，欢迎咨再次询。'
+    else:
+        return '我不知道你在说什么。'
+
+print('test')
