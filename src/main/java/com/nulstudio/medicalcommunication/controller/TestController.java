27e2bbf7d6731ad8d20dd6ba84b373bd5b31e0d9
NULL <jsjtsty@126.com>
2022-10-03 13:26:28 +0800

    Fix Bug #1, 2022.10.3.

--- src/main/java/com/nulstudio/medicalcommunication/controller/TestController.java
+++ src/main/java/com/nulstudio/medicalcommunication/controller/TestController.java
@@ -3,10 +3,12 @@ package com.nulstudio.medicalcommunication.controller;
 import com.nulstudio.medicalcommunication.entity.TestEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TestController {
     @RequestMapping("/")
+    @ResponseBody
     public TestEntity index() {
         return new TestEntity(System.getProperty("user.dir"));
     }
