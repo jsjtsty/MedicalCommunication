f119d9c9828d1d350a08be0f3eb429e3fb57ae40
NULL <jsjtsty@126.com>
2022-10-03 14:23:54 +0800

    Fix Bug #5, 2022.10.3.

--- src/main/java/com/nulstudio/medicalcommunication/controller/TestController.java
+++ src/main/java/com/nulstudio/medicalcommunication/controller/TestController.java
@@ -5,15 +5,40 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 @Controller
 public class TestController {
     @RequestMapping("/")
     @ResponseBody
     public TestEntity index() {
+        final String fileName = "./src/main/python/CommunicationTest.py";
+        StringBuilder s = new StringBuilder();
+        File file = new File(fileName);
+        BufferedReader reader = null;
+        try {
+            System.out.println("以行为单位读取文件内容，一次读一整行：");
+            reader = new BufferedReader(new FileReader(file));
+            String tempString = null;
+            int line = 1;
+            // 一次读入一行，直到读入null为文件结束
+            while ((tempString = reader.readLine()) != null) {
+                // 显示行号
+                s.append("line ").append(line).append(": ").append(tempString);
+                line++;
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e1) {
+                }
+            }
+        }
+        /*
         Process proc;
         StringBuilder s = new StringBuilder();
         try {
@@ -28,7 +53,7 @@ public class TestController {
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
-
+         */
         return new TestEntity(s.toString());
     }
 }
