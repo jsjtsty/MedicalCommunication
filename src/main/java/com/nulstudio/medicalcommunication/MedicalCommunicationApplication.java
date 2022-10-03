0fd14a71330183ce4db0942848afc0ef982cd9ed
NULL <jsjtsty@126.com>
2022-10-03 13:05:32 +0800

    Initial Commit.

---
+++ src/main/java/com/nulstudio/medicalcommunication/MedicalCommunicationApplication.java
@@ -0,0 +1,13 @@
+package com.nulstudio.medicalcommunication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalCommunicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalCommunicationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nulstudio/medicalcommunication/MedicalCommunicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nulstudio.medicalcommunication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalCommunicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
